9546c2e6845c5ec699af60935c0598cf9c929a39
EmanuelErnesto <emanuel.earego@gmail.com>
2024-09-30 19:18:48 -0300

    test: add test file

---
+++ src/test/java/com/project/Atividade/Backend/Framework/AtividadeBackendFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Atividade.Backend.Framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeBackendFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
