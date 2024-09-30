9e2d22122b17a3f48b0733182883a050188b6f9f
EmanuelErnesto <emanuel.earego@gmail.com>
2024-09-30 19:21:38 -0300

    raw: add swagger configuration file

---
+++ src/main/java/com/project/Atividade/Backend/Framework/config/SpringDocOpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.project.Atividade.Backend.Framework.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocOpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Rest API - Atividade Backend framework")
+                                .description("API desenvolvida como requisito para atividade de backend framework.")
+                                .version("v1")
+                                .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0"))
+                                .contact(new Contact().name("Emanuel Ernesto").email("emanuel.earego@gmail.com"))
+                );
+    }
+}
