ce35cacd2a624a42040e725d2587f16d5c2839c5
EmanuelErnesto <emanuel.earego@gmail.com>
2024-09-30 19:22:32 -0300

    feat: add user error messages consts

---
+++ src/main/java/com/project/Atividade/Backend/Framework/shared/consts/UserErrorMessages.java
@@ -0,0 +1,6 @@
+package com.project.Atividade.Backend.Framework.shared.consts;
+
+public class UserErrorMessages {
+    public static final String CPF_ALREADY_USED = "cpf already used";
+    public static final String USER_NOT_FOUND = "user not found";
+}
